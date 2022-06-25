4b9e98f9807533f74da311f1831dbb2c349cce1a
JD <91642296+JohnDenverBalagwis@users.noreply.github.com>
2022-06-25 14:59:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#activity
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+num4 = int(input("Enter the fourth number: "))
+num5 = int(input("Enter the fifth number: "))
+
+sum = (num1 + num2)
+difference = (sum - num3)
+product = (difference * num4)
+qoutient = (product / num5)
+
+print(num1, "+", num2, "=", sum)
+print(sum, "-", num3, "=", difference)
+print(difference, "*", num4, "=", product)
+print(qoutient, "/", num5, "=", qoutient)
